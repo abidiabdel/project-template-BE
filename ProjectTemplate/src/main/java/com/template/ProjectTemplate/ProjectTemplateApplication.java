b2e34b3bd5d1574cc82d765b4e8ad6055acd9f1c
Abdelabset <Abdelbaset.Abidi@studmail.w-hs.de>
2023-10-14 09:08:08 +0200

    Create Product and Catalog Entity

--- ProjectTemplate/src/main/java/com/template/ProjectTemplate/ProjectTemplateApplication.java
+++ ProjectTemplate/src/main/java/com/template/ProjectTemplate/ProjectTemplateApplication.java
@@ -1,13 +1,23 @@
 package com.template.ProjectTemplate;
 
+import com.template.ProjectTemplate.model.Category;
+import com.template.ProjectTemplate.model.Product;
+import com.template.ProjectTemplate.repository.CategoryRepository;
+import com.template.ProjectTemplate.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjectTemplateApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectTemplateApplication.class, args);
+
+
+
+
 	}
 
 }
